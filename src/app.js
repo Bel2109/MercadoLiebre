cf79f3b9bc80296c0c6d731a3e24d65995fe3362
Bel2109 <belenlopezbelen-lopez@outlook.es>
2022-12-14 00:06:33 -0300

    resultado final

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use (express.static('./public'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor levantado en el puerto http://localhost:${port}`));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+app.get("/crea-tu-cuenta", (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/crea-tu-cuenta.html'))
+})
+app.get("/ingresa", (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/ingresa.html'))
+})
